7a7f1437d8c3f1717b0258851d994fb8e8a5ef1e
ARUN-M-S <arun.ms@flexm.com>
2024-06-26 08:21:14 +0530

    flatten array with reduce method

--- arrayMethods/array.js
+++ arrayMethods/array.js
@@ -4,20 +4,27 @@
 
 
 let arr=[1,[2,[3,[4],[5]],[6]],7] 
+// function flattenArray(arr){
+
+//     //Validate is array or not
+//     if(!Array.isArray(arr))return 'Is not an array'
+//     let result=[]
+//     // Loop method + Recurssion
+//     for(let i=0;i<arr.length;i++){
+//         if(Array.isArray(arr[i])){
+//       result=result.concat(flattenArray(arr[i]))
+//         }else{
+//             result.push(arr[i])
+//         }
+//     }
+//     return result
+// }
+
+// with reduce method
+
 function flattenArray(arr){
-
-    //Validate is array or not
-    if(!Array.isArray(arr))return 'Is not an array'
-    let result=[]
-    // Loop method + Recurssion
-    for(let i=0;i<arr.length;i++){
-        if(Array.isArray(arr[i])){
-      result=result.concat(flattenArray(arr[i]))
-        }else{
-            result.push(arr[i])
-        }
-    }
-    return result
+ //Validate is array or not
+ if(!Array.isArray(arr))return 'Is not an array'
+ return arr.reduce((acc,value)=>Array.isArray(value) ? acc.concat(flattenArray(value)):acc.concat(value),[])
 }
-
 console.log(flattenArray(arr))
